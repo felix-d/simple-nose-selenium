98e0449669a1db0fbefd234146a130a09bab997f
Felix Descoteaux <flx.descoteaux@gmail.com>
2017-03-17 14:17:50 -0400

    bumped version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='simple-nose-selenium',
-    version='0.2.0',
+    version='0.2.1',
     author='Felix Descoteaux',
 	author_email = 'flx.descoteaux@gmail.com',
 	description = 'Run your Selenium TestCase\'s in chrome, firefox or saucelabs',
